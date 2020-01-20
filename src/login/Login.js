e0e3dfafa9d75a380a581084a5e6dbf52181cf29
spalavecino <palavecino.sebastian.ariel@gmail.com>
2020-01-20 09:41:22 -0300

    saca boton custom innecesario

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from "../components/button/Button";
 import "./Login.scss";
 
 const Login = () => {
@@ -7,22 +6,16 @@ const Login = () => {
     <main className="Login">
       <div className="container">
         <h3>Ingresar</h3>
-        <Button
-          icon="fab fa-google"
-          text="Sign in with Google"
-          fontColor="#666"
-          background="#fff"
-        />
-        <Button
-          icon="fab fa-facebook-square"
-          text="Sign in with Facebook"
-          background="#3b5998"
-        />
-        <Button
-          icon="far fa-envelope"
-          text="Sign in with Email"
-          background="red"
-        />
+        <button>
+          <i className="fab fa-google"></i> <span>Sign in with Google</span>
+        </button>
+        <button style={{ backgroundColor: "#3b5998" }}>
+          <i className="fab fa-facebook-square"></i>
+          <span>Sign in with Facebook</span>
+        </button>
+        <button>
+          <i className="far fa-envelope"></i> <span>Sign in with email</span>
+        </button>
       </div>
     </main>
   );
