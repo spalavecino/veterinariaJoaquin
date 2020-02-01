d6bd0ca847e0c1c84fefc33a94d5e615a7b8a833
Sebastian <palavecino.sebastian.ariel@gmail.com>
2020-01-31 23:34:10 -0300

    Filtrado de tabla y correcion de bug con firebase

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,12 +2,6 @@ import React from "react";
 import Tr from "./tr";
 
 const Table = ({ columns, data }) => {
-  console.log(columns, data);
-
-  for (let [key, value] of Object.entries(data[0])) {
-    console.log(key, value);
-  }
-
   return (
     <table className="Table">
       <thead>
@@ -18,26 +12,7 @@ const Table = ({ columns, data }) => {
       <tbody>
         {data &&
           data.map((dataRow, index) => {
-            return (
-              <Tr dataRow={dataRow} key={index} />
-              // <tr key={index}>
-              //   {() => {
-              //     let cells = [];
-              //     for (let key in d) {
-              //       if (key != "id") {
-              //         cells.push(<td>d[key]</td>);
-              //       }
-              //     }
-              //     return cells;
-              //   }}
-              // </tr>
-            );
-
-            // return (
-            //   <tr key={d.id}>
-            //     <td>{d.owner}</td>
-            //   </tr>
-            // );
+            return <Tr dataRow={dataRow} key={index} />;
           })}
       </tbody>
     </table>
--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,23 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  // apiKey: process.env.REACT_APP_API_KEY,
+  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  // databaseURL: process.env.REACT_APP_DATABASE_URL,
+  // projectId: process.env.REACT_APP_PROJECTID,
+  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_APP_ID
+  // measurementId: process.env.REACT_APP_MEASUREMENT_ID
+
+  apiKey: "AIzaSyAhJLL83FM0YHuXjjO3B6Q27Tu8AknEH58",
+  authDomain: "clinicaveterinariajoaquin.firebaseapp.com",
+  databaseURL: "https://clinicaveterinariajoaquin.firebaseio.com",
+  projectId: "clinicaveterinariajoaquin",
+  storageBucket: "clinicaveterinariajoaquin.appspot.com",
+  messagingSenderId: "18938073546",
+  appId: "1:18938073546:web:bbfcd55067d7e9fcdbff33"
+
   //   measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
